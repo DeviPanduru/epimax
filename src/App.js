6694ce0c43435225d79d2e371afd412feb626c08
DeviPanduru <devi.panduru@gmail.com>
2024-05-01 19:43:00 +0530

    files are updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class TaskListInterface extends Component {
+  state = {
+    taskName: '',
+    taskDescription: '',
+    dueDate: '',
+    assignee: '',
+    error: ''
+  };
+
+  handleInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const { taskName, taskDescription, dueDate, assignee } = this.state;
+
+    if (!taskName || !taskDescription || !dueDate || !assignee) {
+      this.setState({ error: 'Please fill in all fields' });
+      return;
+    }
+
+    
+    
+    this.setState({
+      taskName: '',
+      taskDescription: '',
+      dueDate: '',
+      assignee: '',
+      error: ''
+    });
+  };
+
+  render() {
+    const { taskName, taskDescription, dueDate, assignee, error } = this.state;
+
+    return (
+      <div className='task-section'>
+      <form d-flex flex-column text-center justify-content-center onSubmit={this.handleFormSubmit}>
+        <input type="text" name="taskName" placeholder="Task Name" value={taskName} onChange={this.handleInputChange} /><br />
+        <textarea name="taskDescription" placeholder="Task Description" value={taskDescription} onChange={this.handleInputChange}></textarea><br />
+        <input type="date" name="dueDate" value={dueDate} onChange={this.handleInputChange} /><br />
+        <input type="text" name="assignee" placeholder="Assignee" value={assignee} onChange={this.handleInputChange} /><br />
+        {error && <div style={{ color: 'red' }}>{error}</div>}
+        <button className='btn btn-primary' type="submit">Add Task</button>
+      </form>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default TaskListInterface;
+
